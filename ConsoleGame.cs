3ed902e0a7ebdf99177b6ddba828040859f2f160
bbks <zizi>
2015-09-22 02:55:20 +0300

    updateGame

--- ConsoleGame.cs
+++ ConsoleGame.cs
@@ -1,95 +1,223 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
+
+struct Object
+{
+    public int x;
+    public int y;
+    public int maxX;
+    public int maxY;
+}
 
 class ConsoleGame
 {
     static void Main()
-    {
-        Console.BufferHeight = Console.WindowHeight = 12;
-        Console.BufferWidth = Console.WindowWidth = 14;
-
-        int x = 0;
-        int y = 0;
+    {   
         int score = 0;
-        int[,] arr =
-        {
-            { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 1, 0, 1, 0 },
-            { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 1, 0, 1, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
-            { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 },
-            { 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 },
-            { 0, 0, 0, 0, 0, 0, 1, 0, 1, 0 }
-        };
+
+        //Declare PlayerCords Obj
+        Object playerCords = new Object();
+
+        //Map Size[x, y] (Minimum Y = 10 !!!)
+        playerCords.maxY = 11;
+        playerCords.maxX = playerCords.maxY * 2;
+
+        //Player's Start Cordinates(In The Middle of MAP)
+        playerCords.x = playerCords.maxX / 2;
+        playerCords.y = playerCords.maxY / 2;
+
+        //Console Size
+        int size = playerCords.maxY;
+        Console.BufferHeight = Console.WindowHeight = size + 7;
+        Console.BufferWidth = Console.WindowWidth = size*2 + 7;
+
+        //Count Of Dollars
+        int CountOfDollars = playerCords.maxY;
+
+        //Print Startup text
+        PrintStartupText();
+
+        //Print Player
+        PrintPlayer(playerCords.x, playerCords.y);
+
+        //List of Cordinates of Dollars
+        List<KeyValuePair<int, int>> dollarsCords = new List<KeyValuePair<int, int>>();
+
+        //Spawn Random Dollars On The Map
+        dollarsCords = SpawnRandomDollars(CountOfDollars, playerCords.maxX, playerCords.maxY);
+
+        //Print Dollars
+        score = PrintDollars(dollarsCords, playerCords.x, playerCords.y, score);
+
+        //Print Score and Frame
+        PrintScoreAndFrame(score, playerCords.maxY, playerCords.maxX);
+
+        Stopwatch collectTimer = new Stopwatch();
+        collectTimer.Start();
+
         while (true)
         {
-            while (Console.KeyAvailable)
+            //While Player doesn't move TRUE ---> else FALSE
+            while (true)
             {
                 ConsoleKeyInfo pressedKey = Console.ReadKey();
 
                 if (pressedKey.Key == ConsoleKey.UpArrow)
                 {
-                    if (y > 0)
+                    if (playerCords.y > 0)
                     {
-                        y--;
+                        playerCords.y--;
+                        break;
                     }
                 }
                 else if (pressedKey.Key == ConsoleKey.DownArrow)
                 {
-                    if (y < 9)
+                    if (playerCords.y < playerCords.maxY)
                     {
-                        y++;
+                        playerCords.y++;
+                        break;
                     }
                 }
                 else if (pressedKey.Key == ConsoleKey.LeftArrow)
                 {
-                    if (x > 0)
+                    if (playerCords.x > 0)
                     {
-                        x--;
+                        playerCords.x--;
+                        break;
                     }
                 }
                 else if (pressedKey.Key == ConsoleKey.RightArrow)
                 {
-                    if (x < 9)
+                    if (playerCords.x < playerCords.maxX)
                     {
-                        x++;
+                        playerCords.x++;
+                        break;
                     }
                 }
-            }
-            if (arr[y, x] == 1)
-            {
-                arr[y, x] = 0;
-                score++;
+                Console.SetCursorPosition(0, 0);
             }
             Console.Clear();
-            Console.SetCursorPosition(x, y);
-            Console.Write("O");
-            Console.SetCursorPosition(0, 10);
-            Console.Write("___________");
-            Console.SetCursorPosition(11, 10);
-            Console.Write("|");
-            Console.SetCursorPosition(0, 11);
-            Console.Write("Score: {0}", score);
-            for (int i = 0; i < arr.GetLength(0); i++)
-            {
-                Console.SetCursorPosition(11, i);
-                Console.Write("|");
-                for (int j = 0; j < arr.GetLength(1); j++)
-                {
-                    if (arr[i, j] == 1)
-                    {
-                        Console.SetCursorPosition(j, i);
-                        Console.Write("$");
 
-                    }
-                }
+            //Print Dollars and Update Score
+            score = PrintDollars(dollarsCords, playerCords.x, playerCords.y, score);
+
+            PrintPlayer(playerCords.x, playerCords.y);
+
+            PrintScoreAndFrame(score, playerCords.maxY, playerCords.maxX);
+
+            if (score == CountOfDollars)
+            {
+                collectTimer.Stop();
+                break;
             }
-            Thread.Sleep(120);
+            Thread.Sleep(5);
+        }
+        PrintYouWin(playerCords.maxY, collectTimer);
+    }
+
+    public static void PrintStartupText()
+    {
+        Console.Write("Collect all the ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("DOLLARS($)\n");
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Controls:");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine(" -LEFT ARROW");
+        Console.WriteLine(" -RIGHT ARROW");
+        Console.WriteLine(" -UP ARROW");
+        Console.WriteLine(" -DOWN ARROW");
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write("PRESS [");
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("ENTER");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("] TO START");
+        Console.ReadLine();
+        Console.Clear();
+    }
+
+    public static void PrintPlayer(int x, int y)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.SetCursorPosition(x, y);
+        Console.Write("O");
+    }
+
+    public static int PrintDollars(List<KeyValuePair<int, int>> list, int x, int y, int score)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        for (int i = 0; i < list.Count; i++)
+        {
+            if (list[i].Key == x && list[i].Value == y)
+            {
+                score++;
+                list.Remove(list[i]);
+                i--;
+            }
+            else
+            {
+                Console.SetCursorPosition(list[i].Key, list[i].Value);
+                Console.Write("$");
+            }
+        }
+        return score;
+    }
+
+    public static void PrintScoreAndFrame(int score, int y, int x)
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        y++;
+        Console.SetCursorPosition(0, y);
+        Console.WriteLine(new string('`', x+2));
+        Console.SetCursorPosition(0, y+1);
+        Console.Write("Collected ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write("DOLLARS");
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.Write(": {0}", score);
+        for (int i = 0; i < y; i++)
+        {
+            Console.SetCursorPosition(x + 1, i);
+            Console.Write("|");
+        }
+    }
+
+    public static List<KeyValuePair<int, int>> SpawnRandomDollars(int n, int maxX, int maxY)
+    {
+        Random random = new Random();
+        int randX;
+        int randY;
+        int count = 0;
+        List<KeyValuePair<int, int>> list = new List<KeyValuePair<int, int>>();
+
+        while (count < n)
+        {
+            randX = random.Next(0, maxX + 1);
+            randY = random.Next(0, maxY + 1);
+            if (!list.Contains(new KeyValuePair<int, int>(randX, randY)))
+            {
+                list.Add(new KeyValuePair<int, int>(randX, randY));
+                count++;
+            }
+        }
+
+        return list;
+    }
+    
+    public static void PrintYouWin(int y, Stopwatch timer)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.SetCursorPosition(0, y + 3);
+        Console.WriteLine("You WIN!\nTime: {0}seconds", timer.Elapsed.Seconds);
+        while (true)
+        {
+            Console.SetCursorPosition(0, y + 5);
+            Console.ReadLine();
         }
     }
 }
